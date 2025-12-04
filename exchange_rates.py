f62ace6528099443902dd99cad3f7d9a4883b225
Dario Nicolas Burbano <da681bur@htwg-konstanz.de>
2025-12-04 16:42:19 +0100

    Changed Database to Neon

--- exchange_rates.py
+++ exchange_rates.py
@@ -44,7 +44,7 @@ PORT = os.getenv("port")
 DBNAME = os.getenv("dbname")
 
 # Construct the SQLAlchemy connection string
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require&channel_binding=require"
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
